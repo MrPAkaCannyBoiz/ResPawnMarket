da129f5fc3f7130aee7b82ce67c7661e409ab8b4
MrPAkaCannyBoiz <pongsatorn5762@gmail.com>
2025-11-03 14:04:09 +0100

    remade CreateCustomer Dto

---
+++ C_sharp/Client/BlazorApp/CustomerItem.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorApp;
+
+public class CustomerItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
--- C_sharp/Client/BlazorApp/obj/Debug/net10.0/BlazorApp.AssemblyInfo.cs
+++ C_sharp/Client/BlazorApp/obj/Debug/net10.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a8d4246b83006b8d73e24dcc83759561e9a60aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+287a7ee93b0b047c8a91e5ec9fb50938342312a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
